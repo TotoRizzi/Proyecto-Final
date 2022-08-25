98ecf9baeb92ad52552829129a61fa0290641cea
TotoRizzi <83927597+TotoRizzi@users.noreply.github.com>
2022-08-25 17:01:39 -0300

    FlyingEnemy
    Enemigo que vuela
    GameManager
    FSM
    IMovement
    PlaceHolderPlayer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public PlaceHolderPlayer player { get { return player; } }
+    public LayerMask wallLayer { get { return wallLayer} }
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    public Vector3 GetDistanceToPlayer(Transform transform)
+    {
+        return player.transform.position - transform.position;
+    }
+}
